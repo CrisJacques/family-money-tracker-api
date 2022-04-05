6ba1d96f1826a501ef99c5e408073f8631a016c6
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-04-05 19:52:35 -0300

    Estrutura do projeto e versão inicial do application.properties

---
+++ src/main/java/com/cristhiane/familymoneytrackerapi/FamilyMoneyTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.cristhiane.familymoneytrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyMoneyTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyMoneyTrackerApiApplication.class, args);
+	}
+
+}
