689d228c66574c2b2fa68c9079809a32bcced888
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-04-20 22:21:39 -0300

    Melhorias na forma de retornar as formas de pagamento para o frontend

--- src/main/java/com/cristhiane/familymoneytrackerapi/controllers/FormaDePagamentoController.java
+++ src/main/java/com/cristhiane/familymoneytrackerapi/controllers/FormaDePagamentoController.java
@@ -1,12 +1,13 @@
 package com.cristhiane.familymoneytrackerapi.controllers;
 
+import java.util.Hashtable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cristhiane.familymoneytrackerapi.enums.FormaDePagamento;
 import com.cristhiane.familymoneytrackerapi.service.FormaDePagamentoService;
 
 @RestController
@@ -16,9 +17,9 @@ public class FormaDePagamentoController {
 	@Autowired
 	FormaDePagamentoService formaDePagamentoService;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<FormaDePagamento[]> findAll() {
-		FormaDePagamento[] list = formaDePagamentoService.findAll(); 
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<?> findAll() {
+		Hashtable<Integer, Object> list = formaDePagamentoService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/cristhiane/familymoneytrackerapi/service/FormaDePagamentoService.java
+++ src/main/java/com/cristhiane/familymoneytrackerapi/service/FormaDePagamentoService.java
@@ -1,12 +1,26 @@
 package com.cristhiane.familymoneytrackerapi.service;
 
+import java.util.Hashtable;
+
 import org.springframework.stereotype.Service;
 
 import com.cristhiane.familymoneytrackerapi.enums.FormaDePagamento;
 
 @Service
-public class FormaDePagamentoService {
-	public FormaDePagamento[] findAll() {
-		return FormaDePagamento.values();
+public class FormaDePagamentoService {	
+	public Hashtable<Integer, Object> findAll() {
+		int i = 0;
+		Hashtable<Integer, Object> formasDePagamento = new Hashtable<Integer, Object>();
+		for (FormaDePagamento fp : FormaDePagamento.values()) {
+			Hashtable<Object, Object> formaDePagamento = new Hashtable<Object, Object>();
+			
+			formaDePagamento.put("cod", fp.getCod());
+			formaDePagamento.put("descricao", fp.getDescricao());
+			
+			formasDePagamento.put(i, formaDePagamento);
+			i++;
+		}
+		
+		return formasDePagamento;
 	}
 }
