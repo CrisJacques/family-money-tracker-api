1ef37cf04844c9f4287e73a70f9f835a3ec9244a
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-05-21 18:31:05 -0300

    Permitindo que o backend que está no Heroku apenas aceite requisições do frontend que está no Heroku

--- src/main/java/com/cristhiane/familymoneytrackerapi/FamilyMoneyTrackerApiApplication.java
+++ src/main/java/com/cristhiane/familymoneytrackerapi/FamilyMoneyTrackerApiApplication.java
@@ -18,8 +18,7 @@ public class FamilyMoneyTrackerApiApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000")
-						.allowedOrigins("https://family-money-tracker-web.herokuapp.com/")
+				registry.addMapping("/**").allowedOrigins("https://family-money-tracker-web.herokuapp.com/")
 						.allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
 		};
